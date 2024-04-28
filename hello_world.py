99b768633ac7ebebe925414d7d1cf69407b1b213
Andree Ayyad <165289100+andreeayyad23@users.noreply.github.com>
2024-04-28 13:16:19 +0300

    Add files via upload
    My First Assignment in GitHub

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+print ("Hello World!")
+
+name = "Andree"
+print ("Hello", name)
+print ("Hello " + name)
+
+name = 7
+print ("Hello", name)
+#print ("Hello "+ name)
+print ("Hello " + str(name))
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}".format(fave_food1,fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}")
